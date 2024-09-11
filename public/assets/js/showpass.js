2770237e2fe0da66a4f86c9e9a28a21da70bcbbb
youssefweb1 <youssefelsabbahy2014@icloud.com>
2024-09-11 10:09:18 +0300

    Remove old files from repository

--- public/assets/js/showpass.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('#togglePassword').click(function() {
-        var passwordField = $('#password');
-        var type = passwordField.attr('type') === 'password' ? 'text' : 'password';
-        passwordField.attr('type', type);
-        $(this).find('i').toggleClass('fa-eye fa-eye-slash');
-    });
-});
