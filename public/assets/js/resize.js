26f17d982e52d71f7fa5dc22a38689abd9c90340
youssefweb1 <youssefelsabbahy2014@icloud.com>
2024-09-14 13:26:05 +0300

    Removed all files from the repository

--- public/assets/js/resize.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('resize', function() {
-    if (window.innerWidth < 900) {
-        alert('You need to use a device with a screen larger than 900px, like an iPad or a laptop.');
-    }
-});
-
-if (window.innerWidth < 900) {
-    alert('You need to use a device with a screen larger than 900px, like an iPad or a laptop.');
-}
